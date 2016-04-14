70e7796bc7c23f9a8f934fa2f4e4cdf906be99b4
Daniel Bader <daniel@i-pol.com>
2016-04-14 11:48:25 +0200

    added floating action button to checkbox and radiobox scene

--- src/scenes/Checkboxes.js
+++ src/scenes/Checkboxes.js
@@ -1,5 +1,8 @@
-import React, { Component, View, ScrollView } from 'react-native';
+import React, { Component, View, ScrollView, StyleSheet } from 'react-native';
 import { Subheader, CheckboxGroup, COLOR } from 'react-native-material-design';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 
 export default class Checkboxes extends Component {
@@ -8,16 +11,18 @@ export default class Checkboxes extends Component {
         const theme = 'paperTeal';
 
         return (
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-                bounces={false}
+
+            <View style={{flex: 1}}>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    bounces={false}
                 >
-                <View>
-                    <Subheader text="Light Theme" />
-                    <CheckboxGroup
-                        primary={theme}
-                        checked={[1, 3]}
-                        items={[{
+                    <View>
+                        <Subheader text="Light Theme" />
+                        <CheckboxGroup
+                            primary={theme}
+                            checked={[1, 3]}
+                            items={[{
                         value: 1, label: 'Checked by default'
                     }, {
                         value: 2, label: 'Default'
@@ -28,14 +33,14 @@ export default class Checkboxes extends Component {
                     }, {
                         value: 5
                     }]}
-                    />
-                    <Subheader text="Dark Theme" />
-                    <View style={{ backgroundColor: COLOR.paperGrey900.color }}>
-                        <CheckboxGroup
-                            primary={theme}
-                            theme="dark"
-                            checked={[1, 3]}
-                            items={[{
+                        />
+                        <Subheader text="Dark Theme" />
+                        <View style={{ backgroundColor: COLOR.paperGrey900.color }}>
+                            <CheckboxGroup
+                                primary={theme}
+                                theme="dark"
+                                checked={[1, 3]}
+                                items={[{
                             value: 1, label: 'Checked by default'
                         }, {
                             value: 2, label: 'Default'
@@ -46,10 +51,32 @@ export default class Checkboxes extends Component {
                         }, {
                             value: 5
                         }]}
-                        />
+                            />
+                        </View>
                     </View>
-                </View>
-            </ScrollView>
+                </ScrollView>
+
+                <ActionButton buttonColor="rgba(231,76,60,1)">
+                    <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+                        <Icon name="android-create" style={styles.actionButtonIcon} />
+                    </ActionButton.Item>
+                    <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+                        <Icon name="android-notifications-none" style={styles.actionButtonIcon} />
+                    </ActionButton.Item>
+                    <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
+                        <Icon name="android-done-all" style={styles.actionButtonIcon} />
+                    </ActionButton.Item>
+                </ActionButton>
+
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    actionButtonIcon: {
+        fontSize: 20,
+        height: 22,
+        color: 'white',
+    },
+});
--- src/scenes/RadioButtons.js
+++ src/scenes/RadioButtons.js
@@ -1,5 +1,7 @@
-import React, { Component, View, ScrollView } from 'react-native';
+import React, { Component, View, ScrollView, StyleSheet } from 'react-native';
 import { Subheader, RadioButtonGroup, COLOR } from 'react-native-material-design';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
 export default class Checkboxes extends Component {
@@ -8,15 +10,17 @@ export default class Checkboxes extends Component {
         const theme = 'paperTeal';
 
         return (
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-                bounces={false}
-            >
-                <Subheader text="Light Theme" />
-                <RadioButtonGroup
-                    primary={theme}
-                    selected={1}
-                    items={[{
+            <View style={{flex:1}}>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    bounces={false}
+                    style={{flex:1}}
+                >
+                    <Subheader text="Light Theme" />
+                    <RadioButtonGroup
+                        primary={theme}
+                        selected={1}
+                        items={[{
                         value: 1, label: 'Selected by default'
                     }, {
                         value: 2, label: 'Default'
@@ -25,14 +29,14 @@ export default class Checkboxes extends Component {
                     }, {
                         value: 4
                     }]}
-                />
-                <Subheader text="Dark Theme" />
-                <View style={{ backgroundColor: COLOR.paperGrey900.color }}>
-                    <RadioButtonGroup
-                        primary={theme}
-                        theme="dark"
-                        selected={1}
-                        items={[{
+                    />
+                    <Subheader text="Dark Theme" />
+                    <View style={{ backgroundColor: COLOR.paperGrey900.color }}>
+                        <RadioButtonGroup
+                            primary={theme}
+                            theme="dark"
+                            selected={1}
+                            items={[{
                             value: 1, label: 'Selected by default'
                         }, {
                             value: 2, label: 'Default'
@@ -41,9 +45,37 @@ export default class Checkboxes extends Component {
                         }, {
                             value: 5
                         }]}
-                    />
-                </View>
-            </ScrollView>
+                        />
+                    </View>
+                </ScrollView>
+
+
+                <ActionButton buttonColor="rgba(231,76,60,1)">
+                    <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+                        <Icon name="android-create" style={styles.actionButtonIcon} />
+                    </ActionButton.Item>
+                    <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+                        <Icon name="android-notifications-none" style={styles.actionButtonIcon} />
+                    </ActionButton.Item>
+                    <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
+                        <Icon name="android-done-all" style={styles.actionButtonIcon} />
+                    </ActionButton.Item>
+                </ActionButton>
+
+            </View>
+
+
+
+
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+    actionButtonIcon: {
+        fontSize: 20,
+        height: 22,
+        color: 'white',
+    },
+});
